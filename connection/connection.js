8b2fcf85e70b732fe048167e03fe615d61f5e38c
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-22 13:25:02 -0300

    criando a connection entre o banco de dados e o servidor

---
+++ connection/connection.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2")
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'worldpad'
+})
+
+module.exports = connection
