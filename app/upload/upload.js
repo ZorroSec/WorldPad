6ba4151c6c095846161fad80c98b9f32acc38dd2
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-24 10:26:25 -0300

    errors

--- app/upload/upload.js
+++ app/upload/upload.js
@@ -4,7 +4,13 @@ const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
         cb(null, 'images/')
     },
-    
+    filename: (req, file, cb)=>{
+        const ext = file.originalname.split('.')[1]
+
+        const newFileName = require("crypto").randomBytes(64).toString('hex')
+
+        cb(null, `${newFileName}.${ext}`)
+    }
 })
 
 const upload = multer({ storage })
