298f261255aca12517aaeff018527d44c41c7653
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2023-12-24 15:43:31 -0300

    comentando todo o arquivo 'upload.js'

--- app/upload/upload.js
+++ app/upload/upload.js
@@ -1,23 +1,23 @@
-const multer = require("multer")
+// const multer = require("multer")
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, 'images/')
-    },
-    filename: (req, file, cb)=>{
-        const ext = file.originalname.split('.')[1]
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb)=>{
+//         cb(null, 'images/')
+//     },
+//     filename: (req, file, cb)=>{
+//         const ext = file.originalname.split('.')[1]
 
-        const newFileName = require("crypto").randomBytes(64).toString('hex')
+//         const newFileName = require("crypto").randomBytes(64).toString('hex')
 
-        cb(null, `${newFileName}.jpg`)
-        // if(ext != 'jpg' && ext != 'jpeg' && ext != 'png'){
-        //     cb(null, `${newFileName}.jpg`)
-        // }else{
-        //     cb(null, `${newFileName}.jpg`)
-        // }
-    }
-})
+//         cb(null, `${newFileName}.jpg`)
+//         // if(ext != 'jpg' && ext != 'jpeg' && ext != 'png'){
+//         //     cb(null, `${newFileName}.jpg`)
+//         // }else{
+//         //     cb(null, `${newFileName}.jpg`)
+//         // }
+//     }
+// })
 
-const upload = multer({ storage })
+// const upload = multer({ storage })
 
 module.exports = upload
